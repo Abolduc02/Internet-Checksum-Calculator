a6dca205a9ed049d45143458a17ecb40c40ed603
Aaron Bolduc <abolduc415@gmail.com>
2022-12-06 20:36:47 -0500

    Add files via upload

---
+++ checksum.py
@@ -0,0 +1,30 @@
+'''
+Title: Internet Checksum Calculator
+Author: Aaron Bolduc
+Date: December 6, 2022
+'''
+
+## Initialize variables used in program
+binaryNumber = 0
+userInput = None
+sum = 0
+
+## Get the checksum length the user wants to calculate
+checksumLength = int(input("Enter checksum length (in bits): "))
+
+## Have user enter in binary numbers they want to include in checksum, save in variable
+while True:
+    userInput = input("Enter binary string, or press enter to continue: ")
+    
+    if userInput == "":
+        break
+
+    binaryNumber += int(userInput, 2)
+
+## Wrap bits and add to number if needed (based on checksum length)
+while binaryNumber > 0:
+    sum += (binaryNumber % (2 ** checksumLength))
+    binaryNumber >>= checksumLength
+
+## Invert bitstring to get final checksum and print out
+print(bin(sum ^ ((2 ** checksumLength) - 1))[2:])
